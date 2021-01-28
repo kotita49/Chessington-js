326740cd2c758f223a24fb701935d11c16562d75
kotita49 <kotita49@gmail.com>
2021-01-28 14:47:49 +0000

    Pawn moves up to 2 squares if it hasnt moved

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -8,14 +8,20 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
+       
         //all the moves of pawn
-        let location = board.findPiece(this)
-        if(this.player === Player.WHITE){
-return Square.at(location.row+1, location.col)
-        }else{
-            return Square.at(location.row-1, location.col)
+        let location = board.findPiece(this);
+       
+        if(location.row ==1 && this.player === Player.WHITE){
+return [Square.at(location.row+2, location.col), Square.at(location.row+1, location.col) ]
+        }else if((location.row !==1 && this.player === Player.WHITE)) {
+            return [Square.at(location.row+1, location.col)]
+        } else if(location.row===6 && this.player===Player.BLACK){
+            return [Square.at(location.row-2, location.col), Square.at(location.row-1, location.col)]
+        } else { 
+            return [Square.at(location.row-1, location.col)]
         }
-        return []
-        //[Square.at(1, 0), Square.at(6, 7)]
+        
     }
 }
+
