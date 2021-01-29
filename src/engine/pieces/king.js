1e05059572ce20ca9fa8314391505af0a90212b9
kotita49 <kotita49@gmail.com>
2021-01-29 14:27:11 +0000

    Added tests for king

--- src/engine/pieces/king.js
+++ src/engine/pieces/king.js
@@ -1,3 +1,4 @@
+import Square from '../square';
 import Piece from './piece';
 
 export default class King extends Piece {
@@ -6,6 +7,20 @@ export default class King extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this);
+        let kingArray = [] ;
+       if(location.row<7){
+           kingArray = kingArray.concat([Square.at(location.row+1, location.col),Square.at(location.row+1, location.col+1),Square.at(location.row+1, location.col-1) ])
+       }
+       if(location.col>0){
+       kingArray = kingArray.concat([ Square.at(location.row, location.col-1), Square.at(location.row, location.col+1)]);
+    }
+    if(location.row>0){
+         kingArray = kingArray.concat([Square.at(location.row-1, location.col), Square.at(location.row-1, location.col+1)])
+        }
+        if(location.row>0 && location.col>0){
+            kingArray = kingArray.concat([Square.at(location.row-1, location.col-1) ])
+        }
+        return kingArray
     }
 }
--- tests/engine/pieces/king.spec.js
+++ tests/engine/pieces/king.spec.js
@@ -1,5 +1,61 @@
 import King from '../../../src/engine/pieces/king';
+import Square from '../../../src/engine/square';
+import Board from '../../../src/engine/board';
+import Player from '../../../src/engine/player';
+import 'chai/register-should';
 
 describe('King', () => {
+    let board;
+    beforeEach(() => board = new Board());    
+    
+    it('can move 1 square vertically', () => {
+        const king = new King(Player.WHITE);
+        board.setPiece(Square.at(0, 4), king);
+        
+        
+        const moves = king.getAvailableMoves(board);
+        
+      // moves.should.have.length(1);
+       moves.should.deep.include.members([Square.at(1, 4)]);
+    });
+
+    it('can move 1 square horizontally ', () => {
+        const king = new King(Player.WHITE);
+        
+        board.setPiece(Square.at(0, 4), king);
+        
+        const moves = king.getAvailableMoves(board);
+        
+      // moves.should.have.length(1);
+       moves.should.deep.include.members([Square.at(0, 5), Square.at(0, 3)]);
+    }); it('can move 1 square diagonally ', () => {
+        const king = new King(Player.WHITE);
+        
+        board.setPiece(Square.at(0, 4), king);
+        
+        const moves = king.getAvailableMoves(board);
+        
+      // moves.should.have.length(1);
+       moves.should.deep.include.members([Square.at(1, 5), Square.at(1, 3)]);
+    });
+
+    it('can have 5 possible moves from the edge of the board', () => {
+        const king = new King(Player.WHITE);
+        
+        board.setPiece(Square.at(0, 4), king);
+        
+        const moves = king.getAvailableMoves(board);
+        
+       moves.should.have.length(5);
+    });
+    it('can have 8 possible moves in the middle of the board', () => {
+        const king = new King(Player.WHITE);
+        
+        board.setPiece(Square.at(2, 4), king);
+        
+        const moves = king.getAvailableMoves(board);
+        
+       moves.should.have.length(8);
+    });
 
 });
